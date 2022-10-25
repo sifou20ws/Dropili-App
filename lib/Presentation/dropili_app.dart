45133a247da81ba577f95bb2265ed56730d92b37
Sifou <blvsifo3@gmail.com>
2022-10-25 23:00:40 +0100

    commit after merge

--- lib/Presentation/dropili_app.dart
+++ lib/Presentation/dropili_app.dart
@@ -1,17 +1,11 @@
-import 'package:dropili/Presentation/home/EditProfilePage/screens/editProfilePage.dart';
-import 'package:dropili/Presentation/home/ProfilePage/screens/profileScreen_page.dart';
 import 'package:dropili/Presentation/localization/app_localization.dart';
 import 'package:dropili/Presentation/localization/bloc/language_bloc.dart';
+import 'package:dropili/Presentation/routing/fade_page_route_builder.dart';
 import 'package:dropili/Presentation/routing/routes.dart';
 import 'package:dropili/common/constant/languages.dart';
 import 'package:dropili/di/get_it.dart';
 import 'package:flutter/material.dart';
 
-import 'package:dropili/Presentation/authentification/forget_password/screens/forget_password_screen.dart';
-import 'package:dropili/Presentation/authentification/Login/screens/login_screen.dart';
-import 'package:dropili/Presentation/authentification/signup/screens/signup_screen.dart';
-import 'package:dropili/Presentation/loading_screen/loadingScreen.dart';
-import 'package:dropili/Presentation/onBoarding/screens/on_boarding_screen.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
--- lib/Presentation/routing/routes.dart
+++ lib/Presentation/routing/routes.dart
@@ -4,7 +4,6 @@ import 'package:dropili/Presentation/authentification/Login/screens/login_screen
 import 'package:dropili/Presentation/authentification/forget_password/screens/forget_password_screen.dart';
 import 'package:dropili/Presentation/authentification/signup/screens/signup_screen.dart';
 import 'package:dropili/Presentation/change_language/change_language_screen.dart';
-import 'package:dropili/Presentation/home/home_screen.dart';
 import 'package:dropili/Presentation/loading_screen/loadingScreen.dart';
 import 'package:dropili/Presentation/onBoarding/screens/on_boarding_screen.dart';
 
@@ -15,7 +14,6 @@ class Routes {
         '/signin': ((context) => LoginScreen()),
         '/register': ((context) => SignupScreen()),
         '/signin/reset': ((context) => ResetPasswordScreen()),
-        '/home': ((context) => HomeScreen()),
         '/changeLanguage': ((context) => ChangeLanguageScreen()),
       };
 }
